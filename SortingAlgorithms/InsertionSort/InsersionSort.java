5092f7543b9320c624de9cf8dfb5404b58ea7d7f
Jayshree <jayshreebshirsat16@gmail.com>
2024-11-27 17:59:14 +0530

    Added insertionSort

---
+++ SortingAlgorithms/InsertionSort/InsersionSort.java
@@ -0,0 +1,31 @@
+package SortingAlgorithms.InsertionSort;
+
+public class InsersionSort {
+
+    public static void main(String[] args) {
+        int arr[] = { 7, 8, 3, 2, 1, 6 };
+        insertionSort(arr);
+        System.out.println();
+        printArray(arr);
+
+    }
+
+    public static void insertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > current) {
+                arr[j + 1] = arr[j];
+                j--;
+
+            }
+            arr[j + 1] = current;
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
