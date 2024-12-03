4e812bcc9f05f10804144cad532dba03b6b82aae
Jayshree <jayshreebshirsat16@gmail.com>
2024-12-03 11:36:39 +0530

    Added QuickSort

--- SortingAlgorithms/QuickSort/QuickSort.java
+++ SortingAlgorithms/QuickSort/QuickSort.java
@@ -1,5 +1,54 @@
 package SortingAlgorithms.QuickSort;
 
 public class QuickSort {
+    public static void main(String[] args) {
+        int arr[] = { 9, 7, 6, 5, 4, 3, 2, 1 };
+        int n = arr.length;
+        quickSort(arr, 0, n - 1);
+        printArray(arr);
+    }
 
+    public static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp;
+        return i;
+
+    }
+
+    public static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            System.out.println(pivotIndex);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
